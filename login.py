8dcd0a508a92c2b06af505bfde8eb5b155187668
Arek Gusciora <bahgusciora@gmail.com>
2020-02-09 19:12:32 +0100

    passwords hashed, 2nd password added to new_acc function

--- login.py
+++ login.py
@@ -1,4 +1,5 @@
 import json
+import getpass
 
 
 def new_acc(login_):
@@ -7,17 +8,23 @@ def new_acc(login_):
         print("This login is already taken.")
         login_ = str(input("New login: "))
     else:
-        password_ = input("Enter the password: ")
-        dic[login_] = password_
-    json.dump(dic, open('credentials.txt', 'w'))
-    print("Your accont has been created!")
+        password_ = getpass.getpass("Enter the password: ")
+        password2 = getpass.getpass("Confirm the password: ")
+        if password_ == password2:
+            dic[login_] = password_
+            json.dump(dic, open('credentials.txt', 'w'))
+            print("Your accont has been created!")
+        else:
+            print("Passwords don't match. Please try again.")
+
+    
 
 def login(login_):
     dic = json.load(open('credentials.txt', 'r'))
     if login_ in dic.keys():
-        password_ = input("Please enter your password: ")
+        password_ = getpass.getpass("Please enter your password: ")
         if password_ == dic.get(login_):
-            print("You have successfully log in.")
+            print("You have successfully logged in.")
         else:
             print("Password is incorrect.")
     else:
@@ -27,14 +34,19 @@ def decision():
     a = str(input("Do you want to log in (1) or create a new account (2)? Please enter '1' or '2': "))
     if a == "1":
         login_ = input("Please enter your login: ")
-        #password_ = input("Please enter your password: ")
         login(login_)
     elif a == "2":
         login_ = input("Choose a login for a new account: ")
         new_acc(login_)
     else:
         x = input("\nEnter '1' if you want to log in or '2' if you want to create a new account. Press 'q' if you want to quit.\n")
-        if x == "q" or x == "Q":
+        if x == "1":
+            login_ = input("Please enter your login: ")
+            login(login_)
+        elif x == "2":
+            login_ = input("Choose a login for a new account: ")
+            new_acc(login_)
+        elif x == "q" or x == "Q":
             return
         else:
             decision()
